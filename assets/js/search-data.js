4d61621e6b51564616b9c282afd2146549064fbb
abteen <abteen.ebrahimi@gmail.com>
2025-07-09 00:06:00 +0000

    Deploying to gh-pages from @ abteen/abteen.github.io@6535ddcfc9b069c5ef5cb7fa25199da446f4b863 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,17 +18,10 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "CV Last Updated: July 8th 2025.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
+            window.location.href = "/assets/pdf/cv_7_8_2025.pdf";
           },
         },{id: "books-the-godfather",
           title: 'The Godfather',
